6d40383718720124d65f6d66f29d60449317c664
Juan Dominguez <juanignaciodom3@gmail.com>
2023-10-09 01:00:22 -0300

    remove dev dependencies

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,6 @@ import App from './core/app';
 
 async function startApp() {
   try {
-    // Local development only
-    if(process.env.NODE_ENV === "local") (await import("dotenv")).config()
-
     const app = new App(process.env.PORT as string);
 
     app.initConfig()
